df1cd75cb0dab04792f18dbab6425a607601c93e
Aaron Sherber <aaron@sherber.com>
2024-03-18 14:57:15 -0400

    Simplify footer

--- tpl/aas.php
+++ tpl/aas.php
@@ -441,8 +441,7 @@ endif;
 			</section>
 			<footer class="container">
 				<div class="row">
-					<h4 class="col-md-5 col-xs-8"><?php echo I18n::_($NAME); ?> <small>- <?php echo I18n::_('Because ignorance is bliss'); ?></small></h4>
-					<p class="col-md-1 col-xs-4 text-center"><?php echo $VERSION; ?></p>
+					<h4 class="col-md-5 col-xs-8"><?php echo I18n::_($NAME); ?> <small>v<?php echo $VERSION; ?></small></h4>
 					<p id="aboutbox" class="col-md-6 col-xs-12">
 						<?php echo sprintf(
                             I18n::_('%s is a minimalist, open source online pastebin where the server has zero knowledge of pasted data. Data is encrypted/decrypted %sin the browser%s using 256 bits AES.',
